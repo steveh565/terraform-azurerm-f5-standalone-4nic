5deb737d7aa53c81b101bc98236718b39cde8048
steveh565 <34660472+steveh565@users.noreply.github.com>
2019-10-01 15:20:57 -0400

    updated image size, reference and plan

--- variables.tf
+++ variables.tf
@@ -28,21 +28,21 @@ variable "nic2_private_ip_address" {}
 variable "nic3_private_ip_address" {}
 variable "nic4_private_ip_address" {}
 variable "vm_size" {
-  default = "Standard_F4"
+  default = "Standard_DS3_v2"
 }
 variable "storage_image_reference" {
   default = {
-    publisher = "fortinet"
-    offer     = "fortinet_fortigate-vm_v5"
-    sku       = "fortinet_fg-vm"
-    version   = "latest"
+    publisher = "f5-networks"
+    offer     = "f5-big-ip-byol"
+    sku       = "f5-big-all-1slot-byol"
+    version   = "14.1.200000"
   }
 }
 variable "plan" {
   default = {
-    name      = "fortinet_fg-vm"
-    publisher = "fortinet"
-    product   = "fortinet_fortigate-vm_v5"
+    name      = "f5-big-all-1slot-byol"
+    publisher = "f5-networks"
+    product   = "f5-big-ip-byol"
   }
 }
 variable "keyvault" {
